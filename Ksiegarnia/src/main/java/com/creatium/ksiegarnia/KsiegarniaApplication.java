a6505ca8be24c64fa04b7373bdf1d032d1a94e3c
Tomek <Tomek@192.168.1.200>
2018-01-08 20:11:14 +0100

    Pierwszy commit projektu księgarnia

---
+++ Ksiegarnia/src/main/java/com/creatium/ksiegarnia/KsiegarniaApplication.java
@@ -0,0 +1,12 @@
+package com.creatium.ksiegarnia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsiegarniaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsiegarniaApplication.class, args);
+	}
+}
